8614a944ac8d332dd4960f3ae887f40baf6c8e2f
opensource1030 <opensource1030@gmail.com>
2016-09-01 11:07:07 -0500

    CP-1459 #Added Nice SSO Error Page

--- app/Auth/Saml2/Saml2Auth.php
+++ app/Auth/Saml2/Saml2Auth.php
@@ -9,10 +9,11 @@
 namespace WA\Auth\Saml2;
 
 use Aacotroneo\Saml2\Saml2Auth as Saml2A;
+use OneLogin_Saml2_Response;
+use DOMDocument;
 
 class Saml2Auth extends Saml2A
 {
-
     /**
      * @var \OneLogin_Saml2_Auth
      */
@@ -33,4 +34,40 @@ class Saml2Auth extends Saml2A
 
         return $auth->login($returnTo, $parameters, $forceAuthn, $isPassive, $stay, $setNameIdPolicy);
     }
+
+    /**
+     * Process a Saml response (assertion consumer service)
+     * When errors are encountered, it returns an array with proper description
+     */    
+    function acs()
+    {
+        $auth = $this->auth;
+
+        $auth->processResponse();
+
+        $errors['errors'] = $auth->getErrors();
+        $errors['errorReason'] = $auth->getLastErrorReason();
+
+        if (!empty($errors['errors'] || !empty($errors['errorReason']))) {
+            $response = new OneLogin_Saml2_Response($auth->getSettings(), $_POST['SAMLResponse']);
+
+            $title = '<h2>Your request has given the next error, please, contact administrator.</h2>';
+            $error = '<h3 style="color:red">'.$errors['errors'][0].' - '.$errors['errorReason'].'</h3>';
+
+            $domxml = new DOMDocument('1.0');
+            $domxml->preserveWhiteSpace = false;
+            $domxml->formatOutput = true;
+            /* @var $xml SimpleXMLElement */
+            $domxml->loadXML($response->response);
+            $result = $domxml->saveXML();
+
+            return $title.$error.'<pre>'.htmlspecialchars($result).'</pre>';
+        }
+
+        if (!$auth->isAuthenticated()) {
+            return array('error' => 'Could not authenticate');
+        }
+
+        return null;
+    }    
 }
