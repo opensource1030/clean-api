369aaad7c01375d60fdb473a6a13b4e00b252079
opensource1030 <opensource1030@gmail.com>
2016-09-04 14:28:31 -0400

    CP-1460 #message Tests for filters and sorting

--- tests/unit/ApiFiltersTest.php
+++ tests/unit/ApiFiltersTest.php
@@ -8,4 +8,28 @@ class ApiFiltersTest extends TestCase
 
     use DatabaseTransactions;
 
+    public function testCanIncludeFiltersInMeta()
+    {
+        $this->json('GET', '/devices?filter[identification]=296')
+            ->seeJson(['filter' => ['[identification]=296']]);
+    }
+
+    public function testCanIncludeMultipleFiltersInMeta()
+    {
+        $this->json('GET', '/devices?filter[identification]=296&filter[id]=15')
+            ->seeJson(['filter' => ['[identification]=296', '[id]=15']]);
+    }
+
+    public function testCanIncludeFiltersWithDelimittedCriteriaInMeta()
+    {
+        $this->json('GET', '/devices?filter[id]=2,4')
+            ->seeJson(['filter' => ['[id]=2,4']]);
+    }
+
+    // Make sure we ignore invalid criteria.  Replace with error check if we decide that's better.
+    public function testWorkProperlyWithIncorrectSortCriteria()
+    {
+        $response = $this->call('GET', '/devices?filter[blahblah]=9001');
+        $this->assertEquals(200, $response->status());
+    }
 }
--- tests/unit/ApiSortingTest.php
+++ tests/unit/ApiSortingTest.php
@@ -8,4 +8,45 @@ class ApiSortingTest extends TestCase
 
     use DatabaseTransactions;
 
+    public function testCanIncludeSortInMeta()
+    {
+        $this->json('GET', '/devices?sort=-identification')
+            ->seeJson(['sort' => '-identification']);
+    }
+
+    public function testCanSortResource()
+    {
+        $response = $this->call('GET', '/devices?sort=identification');
+        $json = json_decode($response->getContent());
+        $sorted = [];
+        foreach ($json->data as $row) {
+            $sorted[] = $row->attributes->identification;
+        }
+        $resorted = $sorted;
+        sort($resorted);
+        $this->assertEquals($resorted, $sorted);
+    }
+
+    public function testCanInvertSortResource()
+    {
+        $response = $this->call('GET', '/devices?sort=-identification');
+        $json = json_decode($response->getContent());
+        $sorted = [];
+        foreach ($json->data as $row) {
+            $sorted[] = $row->attributes->identification;
+        }
+        $resorted = $sorted;
+        sort($resorted);
+
+        // Now the first element of the resorted array should match the last element of the result array
+        $this->assertEquals($resorted[0], end($sorted));
+    }
+
+
+    // Make sure we ignore invalid criteria.  Replace with error check if we decide that's better.
+    public function testWorkProperlyWithIncorrectSortCriteria()
+    {
+        $response = $this->call('GET', '/devices?sort=-blahblah');
+        $this->assertEquals(200, $response->status());
+    }
 }
