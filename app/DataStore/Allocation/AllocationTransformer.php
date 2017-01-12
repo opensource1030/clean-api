3ded9008c31071b40ddb62797603bfb98f248481
opensource1030 <opensource1030@gmail.com>
2017-01-12 18:50:41 -0500

    CP-1680 Extend the allocations endpoint to included required data

--- app/DataStore/Allocation/AllocationTransformer.php
+++ app/DataStore/Allocation/AllocationTransformer.php
@@ -28,6 +28,20 @@ class AllocationTransformer extends FilterableTransformer
             'equipment'           => (float)$allocations->equipmentCost,
             'other_category'      => (float)$allocations->otherCategory,
             'unknown_category'    => (float)$allocations->unknownCategory,
+            'device_type'         => (float)$allocations->deviceType,
+            'domestic_usage'      => (float)$allocations->domesticUsageCharge,
+            'domestic_data'       => (float) $allocations->domesticDataUsage,
+            'domestic_voice'      => (float) $allocations->domesticVoiceUsage,
+            'domestic_text'       => (float) $allocations->domesticText,
+            'intl_roam_usage'     => (float) $allocations->intlUsage,
+            'int_roam_data'       => (float) $allocations->intlRoamData,
+            'intl_roam_voice'     => (float) $allocations->intlRoamVoice,
+            'intl_roam_text'      => (float) $allocations->intlRoamText,
+            'intl_ld_usage'       => (float) $allocations->intlLDUsage,
+            'intl_ld_voice'       => (float) $allocations->intlLDVoice,
+            'intl_ld_text'        => (float) $allocations->intlLDText,
+            'etf'                 => (float) $allocations->etf,
+            'other_carrier_charges' => (float) $allocations->otherCarrierCharges,
         ];
     }
 }
