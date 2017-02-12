107ba9936af5fe4a56b2fae038d5e2863a635121
opensource1030 <opensource1030@gmail.com>
2017-02-11 22:02:12 -0500

    CP-1677 #message Add some tests

---
+++ tests/wa/http/controllers/FilteredApiControllerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace WA\Testing\Http\Controllers;
+
+use TestCase;
+
+class FilteredApiControllerTest extends TestCase
+{
+
+    public function testCanConstructAnExtendedController() {
+        $controller = app()->make('WA\Http\Controllers\DevicesController');
+        $this->assertInstanceOf(\WA\Http\Controllers\FilteredApiController::class,$controller);
+    }
+}
