30a834b8f4dc381fbf134c5a4369083f2f474cd1
opensource1030 <opensource1030@gmail.com>
2017-04-19 04:18:28 -0500

    CP-1737 #Place Order Packages-Services Seeders

--- database/seeds/PackagesTableSeeder.php
+++ database/seeds/PackagesTableSeeder.php
@@ -37,6 +37,146 @@ class PackagesTableSeeder extends BaseTableSeeder
             [
                 'name'     => "Package5",
                 'companyId' => 1
+            ],
+            [
+                'name'     => "Package1",
+                'companyId' => 20
+            ],
+            [
+                'name'     => "Package2",
+                'companyId' => 20
+            ],
+            [
+                'name'     => "Package3",
+                'companyId' => 20
+            ],
+            [
+                'name'     => "Package4",
+                'companyId' => 20
+            ],
+            [
+                'name'     => "Package5",
+                'companyId' => 20
+            ],
+            [
+                'name'     => "Package6",
+                'companyId' => 20
+            ],
+            [
+                'name'     => "Package7",
+                'companyId' => 20
+            ],
+            [
+                'name'     => "Package8",
+                'companyId' => 20
+            ],
+            [
+                'name'     => "Package9",
+                'companyId' => 20
+            ],
+            [
+                'name'     => "Package10",
+                'companyId' => 20
+            ],
+            [
+                'name'     => "Package11",
+                'companyId' => 20
+            ],
+            [
+                'name'     => "Package12",
+                'companyId' => 20
+            ],
+            [
+                'name'     => "Package13",
+                'companyId' => 20
+            ],
+            [
+                'name'     => "Package14",
+                'companyId' => 20
+            ],
+            [
+                'name'     => "Package15",
+                'companyId' => 20
+            ],
+            [
+                'name'     => "Package16",
+                'companyId' => 20
+            ],
+            [
+                'name'     => "Package17",
+                'companyId' => 20
+            ],
+            [
+                'name'     => "Package18",
+                'companyId' => 20
+            ],
+            [
+                'name'     => "Package19",
+                'companyId' => 20
+            ],
+            [
+                'name'     => "Package20",
+                'companyId' => 20
+            ],
+            [
+                'name'     => "Package21",
+                'companyId' => 20
+            ],
+            [
+                'name'     => "Package22",
+                'companyId' => 20
+            ],
+            [
+                'name'     => "Package23",
+                'companyId' => 20
+            ],
+            [
+                'name'     => "Package24",
+                'companyId' => 20
+            ],
+            [
+                'name'     => "Package25",
+                'companyId' => 20
+            ],
+            [
+                'name'     => "Package26",
+                'companyId' => 20
+            ],
+            [
+                'name'     => "Package27",
+                'companyId' => 20
+            ],
+            [
+                'name'     => "Package28",
+                'companyId' => 20
+            ],
+            [
+                'name'     => "Package29",
+                'companyId' => 20
+            ],
+            [
+                'name'     => "Package30",
+                'companyId' => 20
+            ],
+            [
+                'name'     => "Package31",
+                'companyId' => 20
+            ],
+            [
+                'name'     => "Package32",
+                'companyId' => 20
+            ],
+            [
+                'name'     => "Package33",
+                'companyId' => 20
+            ],
+            [
+                'name'     => "Package34",
+                'companyId' => 20
+            ],
+            [
+                'name'     => "Package35",
+                'companyId' => 20
             ]
         ];
 
